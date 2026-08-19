07b6de2fdeeb30645721f3b95d0c2f730dcf5448
Mudher84 <94250332+Mudher84@users.noreply.github.com>
2026-08-19 12:03:41 +0300

    ui: install celestial prayer arc marker

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import SakinahResponsiveShell from './SakinahResponsiveShell.jsx'
 import { installTypographyBoost } from './typographyBoost.js'
+import { installCelestialArc } from './celestialArc.js'
 import './index.css'
 import './amiriExperiment.css'
 
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 )
 
 installTypographyBoost()
+installCelestialArc()
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => navigator.serviceWorker.register("/sw.js").catch(()=>{}));
