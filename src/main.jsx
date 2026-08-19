a3df76a72a002a306550caef154d41e8ed63c084
Mudher84 <94250332+Mudher84@users.noreply.github.com>
2026-08-19 07:51:52 +0300

    Run merged Sakinah runtime

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import MergedSakinah from './MergedSakinah.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <MergedSakinah />
   </React.StrictMode>,
 )
 
