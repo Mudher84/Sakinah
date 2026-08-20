7c8906097f1259be7612b62b001443f5f48d01de
Mudher84 <94250332+Mudher84@users.noreply.github.com>
2026-08-20 20:58:38 +0300

    Load global scrollbar hiding

--- src/main.jsx
+++ src/main.jsx
@@ -29,6 +29,7 @@ import './topbarBrandPulse.css'
 import './playerControlsCenter.css'
 import './dailyCardMotion.css'
 import './profilePagePolish.css'
+import './globalScrollbar.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
