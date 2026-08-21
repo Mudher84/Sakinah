bc3fbb2dd60052ee4ae0d3485e9a9bf39cd5905d
Mudher84 <94250332+Mudher84@users.noreply.github.com>
2026-08-21 13:01:58 +0300

    Load compact Quran surah card styling

--- src/main.jsx
+++ src/main.jsx
@@ -31,6 +31,7 @@ import './globalScrollbar.css'
 import './beatlyMirror.css'
 import './quranTopTabsFix.css'
 import './quranSurahNumberBadges.css'
+import './quranSurahCompact.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
