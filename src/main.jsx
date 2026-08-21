47ace3546188e08fa58f5ad0ae5c8b259a557c11
Mudher84 <94250332+Mudher84@users.noreply.github.com>
2026-08-21 15:10:00 +0300

    Enable prayer timings fallback before home mounts

--- src/main.jsx
+++ src/main.jsx
@@ -17,6 +17,7 @@ import { installLivingHomeExperience } from './livingHomeExperience.js'
 import { installLivingHomeRotation } from './livingHomeRotation.js'
 import { installAlyamFooter } from './alyamFooter.js'
 import { installBrandIdentity } from './brandIdentity.js'
+import { installGeolocationFallback } from './geolocationFallback.js'
 import { installQuranLiveWaveform } from './quranLiveWaveform.js'
 import { installQuranSurahNumberGuard } from './quranSurahNumberGuard.js'
 import { installQuranSurahAutoPlay } from './quranSurahAutoPlay.js'
@@ -40,6 +41,8 @@ import './quranPlayerControlsPolish.css'
 import './quranDockSafeSpace.css'
 import './dockSlimGlass.css'
 import './globalTypography.css'
+
+installGeolocationFallback()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
